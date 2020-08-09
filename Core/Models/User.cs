0f10d7af838ce4521c1ba61e540b4a2774df3326
Joan Titin <joanmanueltitin@gmail.com>
2020-08-09 19:57:44 -0400

    Add project files.

---
+++ Core/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.Models
+{
+    public sealed class User
+    {
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string IdentificationNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNuber { get; set; }
+
+        public string Address { get; set; }
+    }
+}
